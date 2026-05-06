5edec8bb0db296bd6936617901975766e0117b49
mchaudhry02 <mchaudhry20@hotmail.com>
2026-05-06 12:49:53 -0500

    updated calculator and stringutilites

--- src/main/java/org/example/StringUtilities.java
+++ src/main/java/org/example/StringUtilities.java
@@ -3,14 +3,31 @@ package org.example;
 public class StringUtilities {
     // Refer to Java Basics 1 for common string methods
 
-    // Add a static function called shortString that takes a String parameter and returns true if the string has fewer than 5
+    // Add a static function called shortString that takes a String parameter and
+    // returns true if the string has fewer than 5
     // characters
+    public static boolean shortString(String s) {
+        return s.length() < 5;
+    }
 
-    // Add a static function called firstLetter that takes a String parameter and returns the first character of the string
-
-    // Add a static function called censorAsparagus that takes a String parameter and returns the string with all instances of
+    // Add a static function called firstLetter that takes a String parameter and
+    // returns the first character of the string
+    public static char firstLetter(String s) {
+        return s.charAt(0);
+    }
+    // Add a static function called censorAsparagus that takes a String parameter
+    // and returns the string with all instances of
     // "asparagus" (lowercase only) replaced with 4 stars: "****"
-
-    // Add a static function called bigger that takes two String parameters and returns the longer of the two strings. If the
+    public static String censorAsparagus(String s) {
+        return s.replace("asparagus", "****");
+    }
+    // Add a static function called bigger that takes two String parameters and
+    // returns the longer of the two strings. If the
     // strings are the same length, return the first string.
+    public static String bigger(String a, String b) {
+        if (b.length() > a.length()) {
+            return b;
+        }
+        return a;
+    }
 }
